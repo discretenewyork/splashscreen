e78c37617e993e665711be92b5acc5ec0059288f
Elias Martinez Cohen <eliasmartinezcohen@gmail.com>
2017-11-08 13:03:38 -0500

    Refactor into src/ folder

--- script/build.js
+++ script/build.js
@@ -3,25 +3,27 @@ var stealTools = require('steal-tools')
 var fs = require('fs')
 
 const rootDir = path.resolve(__dirname, '..')
+const srcDir = path.resolve(rootDir, 'src')
 const distDir = path.join(rootDir, 'dist')
 const config = path.join(rootDir, 'package.json!npm')
+
 const index = {
-  src: path.join(rootDir, 'index.prod.html'),
+  src: path.join(srcDir, 'index.prod.html')
   dest: path.join(distDir, 'index.html')
 }
 const favicon = {
-  src: path.join(rootDir, 'favicon.png'),
+  src: path.join(srcDir, 'favicon.png'),
   dest: path.join(distDir, 'favicon.png')
 }
 
 stealTools.optimize({ config }, {
   sourceMap: true,
-  minify: true,
-  target: 'web',
-  envify: true
+  taget: 'web',
+  envify: true,
+  minify: true
 }).then(() => {
-  fs.copyFileSync(index.src, index.dest)
-  fs.copyFileSync(favicon.src, favicon.dest)
+  fs.copyFile(index.src, index.dest)
+  fs.copyFile(favicon.src, favicon.dest)
 }, e => {
-  console.log('ERROR', e)
+  console.log('ERROR:', e)
 })
