afae2f4a380fe79330a380e5127931abab3c9378
Elias Martinez Cohen <eliasmartinezcohen@gmail.com>
2017-11-08 16:52:22 -0500

    Add website copy text

---
+++ src/copyText.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+/* eslint-disable max-len */
+export const interns = [
+  <p key='interns1'>
+    Discrete is seeking smart, capable, and self-motivated interns to join us
+    in our Manhattan office. Currently, we have openings in business
+    administrative and social media roles.
+  </p>,
+  <p key='interns2'>
+    Interested candidates should send a resume and cover letter to
+    apply@discretenewyork.com.
+  </p>
+]
+
+export const about = [
+  <p key='about1'>
+    Discrete New York is a private, independent collective founded by an
+    orthogonal group of individuals from creative, financial, technological,
+    and luxury backgrounds.
+  </p>,
+  <p key='about2'>
+    Headquartered in Manhattan with a deep heritage in luxury vehicles,
+    Discrete fosters efficient, strategic connections between members to
+    capitalize on corollary innovations in the global market.
+  </p>
+]
