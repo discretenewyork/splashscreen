8a15bac314c9a46da4dcbb47c4017384784252a2
Elias Martinez Cohen <eliasmartinezcohen@gmail.com>
2017-11-07 19:47:53 -0500

    Add production build script

---
+++ script/build-prod.js
@@ -0,0 +1,27 @@
+var path = require('path')
+var stealTools = require('steal-tools')
+var fs = require('fs')
+
+const rootDir = path.resolve(__dirname, '..')
+const distDir = path.join(rootDir, 'dist')
+const config = path.join(rootDir, 'package.json!npm')
+const index = {
+  src: path.join(rootDir, 'index.prod.html'),
+  dest: path.join(distDir, 'index.html')
+}
+const favicon = {
+  src: path.join(rootDir, 'favicon.png'),
+  dest: path.join(distDir, 'favicon.png')
+}
+
+stealTools.optimize({ config }, {
+  sourceMap: true,
+  minify: true,
+  target: 'web',
+  envify: true
+}).then(() => {
+  fs.copyFileSync(prodHtml, destHtml)
+  fs.copyFileSync(favicon, favDest)
+}, e => {
+  console.log('ERROR', e)
+})
