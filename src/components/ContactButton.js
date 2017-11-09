7d32b9077681483e296604fd787f4f4b878581f5
Elias Martinez Cohen <eliasmartinezcohen@gmail.com>
2017-11-08 23:17:48 -0500

    Fix email input bug in mobile UX

--- src/components/ContactButton.js
+++ src/components/ContactButton.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import MobileDetect from 'mobile-detect'
 
 const SelectedText = styled.input`
   border: 1px solid ${props => props.theme.color.text};
@@ -15,6 +16,9 @@ const SelectedText = styled.input`
   text-align: center;
 `
 
+function make() {
+  return `${this.a}@${this.b}.${this.c}`
+}
 
 export default class ContactButton extends Component {
   constructor(props) {
@@ -25,10 +29,10 @@ export default class ContactButton extends Component {
     this.a = 'info'
     this.b = 'discretenewyork'
     this.c = 'com'
+    this.detect = new MobileDetect(window.navigator.userAgent)
 
     this.hideEmail = ::this.hideEmail
     this.showEmail = ::this.showEmail
-    this.selectAndCopy = ::this.selectAndCopy
   }
 
   hideEmail() {
@@ -37,40 +41,45 @@ export default class ContactButton extends Component {
 
   showEmail() {
     this.setState(() => ({ emailShown: true }), () => {
-      this.input.focus()
+      if (this.input) {
+        this.input.focus()
+        this.input.select()
+        try {
+          document.execCommand('copy')
+        } catch (e) {
+          //eslint-disable-next-line no-console
+          console.error(e)
+        }
+      }
     })
-  }
-
-  selectAndCopy() {
-    this.input.select()
-    try {
-      document.execCommand('copy')
-    } catch (e) {
-      //eslint-disable-next-line no-console
-      console.error(e)
-    }
   }
 
   render() {
     const show = this.state.emailShown
+    let email
+    if (this.detect.mobile()) {
+      email = (
+        <a href={`mailto:${this::make()}`}>
+          {this::make()}
+        </a>)
+    } else {
+      email = (
+        <SelectedText
+          defaultValue={`${this::make()}`}
+          innerRef={ref => { this.input = ref }}
+          onBlur={this.hideEmail}
+          size={23} />)
+    }
     return (
       <li
         style={{ background: show ? 'white' : 'none' }}>
-        {show ? (
-          <SelectedText
-            defaultValue={`${this.a}@${this.b}.${this.c}`}
-            innerRef={ref => { this.input = ref }}
-            onBlur={this.hideEmail}
-            onFocus={this.selectAndCopy}
-            size={23} />
-        ) : (
+        {show ? email : (
           <a
             onClick={this.showEmail}
             href='#contact'>
             Contact
           </a>
         )}
-      </li>
-    )
+      </li>)
   }
 }
